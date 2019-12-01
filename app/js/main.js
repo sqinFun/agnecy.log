5898d152814ba303806477d6323ff789fa71ffe0
sqinFun <denchyg@yandex.ru>
2019-12-01 23:48:51 +0300

    Добавил навигацию и первую секцию

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
+    function activated(activeItem) {
+        $(activeItem).click(function(){
+            $(this).parent().children().removeClass("active");
+            $(this).addClass("active");
+        })
+    }
 
+    activated(".navbar__elem");
 
 
   
