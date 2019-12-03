d821d14e3feef95019c80930812d12002be77f7e
sqinFun <denchyg@yandex.ru>
2019-12-03 23:41:34 +0300

    Исправил detal-gallery

--- app/js/main.js
+++ app/js/main.js
@@ -13,13 +13,6 @@ $(function(){
     function detalGallery(classElem) {
         activated(classElem+"__elem");
         $(classElem).click(function(){
-            /*
-            <img class="detal-gallery__img-active" src="img/detalGallery/1.jpg">
-            <div class="detal-gallery__text-active">
-                <h2>mark waugh</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed dooing eiusmod tempor incididut labore Ui / Ux , print template.</p>
-            </div>
-            */
             let className = classElem.slice(1);
             $(classElem+"__img-active").remove();
             $(classElem+"__text-active").remove();
@@ -27,7 +20,6 @@ $(function(){
                 textActive = $(classElem+"__elem.active").children(classElem+"__text").clone().removeClass(className+"__text").addClass(className+"__text-active");
             $(classElem).prepend(textActive).prepend(imgActive);
         })
-
     }
 
     detalGallery(".detal-gallery")
